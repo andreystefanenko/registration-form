57ab8825cff890b79770a191bd42afdb0ee5757f
a.stefanenko <73231297+lolechek99@users.noreply.github.com>
2021-06-15 01:51:42 +0300

    Rename process for heroku.

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.json({extended: true}))
 //register routes
 app.use('/api/auth', require('./routes/auth.routes'))
 
-const PORT = procces.env.PORT || config.get('port') || 5000
-
+const PORT = process.env.PORT || config.get('port') || 5000
+git
 async function start (){
     try{
         console.log(JSON.stringify(process.env))
--- client/src/pages/TablePage.js
+++ client/src/pages/TablePage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const TablePage = () => {
     return (
         <div>
-            <h1>ЗДЕСЬ ДОЛЖНА БЫТЬ БЛЯДСКАЯ ТАБЛИЦА</h1>
+            <h1>table</h1>
         </div>
     )
 }
