b1af1bab72b60adc9f86e7329f17e269b29c0d67
a.stefanenko <73231297+lolechek99@users.noreply.github.com>
2021-06-15 02:12:46 +0300

    Added index strings.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 const app = express()
-
+const path = require('path');
 app.use(express.json({extended: true}))
 //register routes
 app.use('/api/auth', require('./routes/auth.routes'))
+
+
 
 const PORT = process.env.PORT || config.get('port') || 5000
 async function start (){
@@ -20,6 +22,11 @@ async function start (){
             useCreateIndex: true
         })
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
     }
     catch (e) {
         console.log('Server Error', e.message)
