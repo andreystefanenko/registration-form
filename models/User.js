99d628474b939ff8edb2fcecc6a36acb64e31139
a.stefanenko <73231297+lolechek99@users.noreply.github.com>
2021-06-15 04:02:49 +0300

    Update schema user.

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     regDate: {type: Date},
+    lastLogin: {type: Date}
 })
 
 module.exports = model('User', schema)
